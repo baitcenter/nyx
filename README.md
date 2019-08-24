f8d6fa7f21b45c9e467e57999cd0eefc85052d3c
evenorog <evenorog@gmail.com>
2019-08-24 21:07:23 +0200

    Move iterator functions to separate iter module

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Provides functions for finding the amount of bytes that are processed per second.
 
 ```rust
-nyx::to_stdout(0..1_000_000_u64).for_each(|_| ())
+nyx::iter::to_stdout(0..1_000_000_u64).for_each(|_| ())
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,10 @@
 //! Provides functions for finding the amount of bytes that are processed per second.
 //!
 //! ```
-//! nyx::to_stdout(0..1_000_000_u64).for_each(|_| ())
+//! nyx::iter::to_stdout(0..1_000_000_u64).for_each(|_| ())
 //! ```
 
 use std::fmt::{self, Display, Formatter};
-use std::iter::Map;
-use std::mem;
-use std::sync::mpsc::Sender;
-use std::time::Instant;
 
 /// Bytes per second.
 ///
@@ -42,79 +38,87 @@ impl Display for Bps {
     }
 }
 
-/// Returns an iterator that provides the bytes per second by printing it to `stdout`.
-///
-/// # Examples
-/// ```
-/// nyx::to_stdout(0..1_000_000_u64).for_each(|_| ())
-/// ```
-#[inline]
-pub fn to_stdout<I, T>(
-    iter: impl IntoIterator<Item = T, IntoIter = I>,
-) -> Map<I, impl FnMut(T) -> T>
-where
-    I: Iterator<Item = T>,
-{
-    to_slot(iter, |bps| println!("{}", bps))
-}
+pub mod iter {
+    use super::Bps;
+    use std::iter::Map;
+    use std::mem;
+    use std::sync::mpsc::Sender;
+    use std::time::Instant;
 
-/// Returns an iterator that provides the bytes per second by calling the provided function.
-///
-/// # Examples
-/// ```
-/// # use nyx::Bps;
-/// nyx::to_slot(0..1_000_000_u64, |bps| match bps {
-///     Bps(0) => eprintln!("N/A"),
-///     bps => println!("{}", bps),
-/// }).for_each(|_| ())
-/// ```
-#[inline]
-pub fn to_slot<I, T>(
-    iter: impl IntoIterator<Item = T, IntoIter = I>,
-    mut f: impl FnMut(Bps),
-) -> Map<I, impl FnMut(T) -> T>
-where
-    I: Iterator<Item = T>,
-{
-    let mut bytes = 0;
-    let mut instant = Instant::now();
-    iter.into_iter().map(move |item| {
-        bytes += mem::size_of_val(&item) as u64;
-        let elapsed = instant.elapsed();
-        if elapsed.as_secs() != 0 {
-            instant = Instant::now();
-            f(Bps((bytes as f64 / elapsed.as_secs_f64()) as u64));
-            bytes = 0;
-        }
-        item
-    })
-}
+    /// Returns an iterator that provides the bytes per second by printing it to `stdout`.
+    ///
+    /// # Examples
+    /// ```
+    /// nyx::iter::to_stdout(0..1_000_000_u64).for_each(|_| ())
+    /// ```
+    #[inline]
+    pub fn to_stdout<I, T>(
+        iter: impl IntoIterator<Item = T, IntoIter = I>,
+    ) -> Map<I, impl FnMut(T) -> T>
+    where
+        I: Iterator<Item = T>,
+    {
+        to_slot(iter, |bps| println!("{}", bps))
+    }
 
-/// Returns an iterator that provides the bytes per second by sending it through the provided `Sender`.
-///
-/// # Examples
-/// ```
-/// use std::sync::mpsc;
-/// use std::thread;
-///
-/// let (sender, receiver) = mpsc::channel();
-/// thread::spawn(move || {
-///     nyx::to_sender(0..1_000_000_u64, sender).for_each(|_| ())
-/// });
-///
-/// for bps in receiver.iter() {
-///     println!("{}", bps);
-/// }
-/// ```
-#[inline]
-pub fn to_sender<I, T>(
-    iter: impl IntoIterator<Item = T, IntoIter = I>,
-    sender: Sender<Bps>,
-) -> Map<I, impl FnMut(T) -> T>
-where
-    I: Iterator<Item = T>,
-{
-    to_slot(iter, move |bps| {
-        let _ = sender.send(bps);
-    })
+    /// Returns an iterator that provides the bytes per second by calling the provided function.
+    ///
+    /// # Examples
+    /// ```
+    /// # use nyx::Bps;
+    /// nyx::iter::to_slot(0..1_000_000_u64, |bps| match bps {
+    ///     Bps(0) => eprintln!("N/A"),
+    ///     bps => println!("{}", bps),
+    /// }).for_each(|_| ())
+    /// ```
+    #[inline]
+    pub fn to_slot<I, T>(
+        iter: impl IntoIterator<Item = T, IntoIter = I>,
+        mut f: impl FnMut(Bps),
+    ) -> Map<I, impl FnMut(T) -> T>
+    where
+        I: Iterator<Item = T>,
+    {
+        let mut bytes = 0;
+        let mut instant = Instant::now();
+        iter.into_iter().map(move |item| {
+            bytes += mem::size_of_val(&item) as u64;
+            let elapsed = instant.elapsed();
+            if elapsed.as_secs() != 0 {
+                instant = Instant::now();
+                f(Bps((bytes as f64 / elapsed.as_secs_f64()) as u64));
+                bytes = 0;
+            }
+            item
+        })
+    }
+
+    /// Returns an iterator that provides the bytes per second by sending it through the provided `Sender`.
+    ///
+    /// # Examples
+    /// ```
+    /// use std::sync::mpsc;
+    /// use std::thread;
+    ///
+    /// let (sender, receiver) = mpsc::channel();
+    /// thread::spawn(move || {
+    ///     nyx::iter::to_sender(0..1_000_000_u64, sender).for_each(|_| ())
+    /// });
+    ///
+    /// for bps in receiver.iter() {
+    ///     println!("{}", bps);
+    /// }
+    /// ```
+    #[inline]
+    pub fn to_sender<I, T>(
+        iter: impl IntoIterator<Item = T, IntoIter = I>,
+        sender: Sender<Bps>,
+    ) -> Map<I, impl FnMut(T) -> T>
+    where
+        I: Iterator<Item = T>,
+    {
+        to_slot(iter, move |bps| {
+            let _ = sender.send(bps);
+        })
+    }
 }
